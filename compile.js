9c0fb776bc7057eccc59837becaf325d0b6ec080
nsward <wardnick@protonmail.com>
2019-06-23 19:39:05 -0600

    Add proxy_storage -- loads delegatecall target from storage

--- compile.js
+++ compile.js
@@ -4,31 +4,32 @@ const fs = require('fs');
 const parser = require('./huff/src/parser');
 const compiler = require('./huff/src/compiler');
 
-const pathToData = path.posix.resolve(__dirname, './huff_modules');
+const modulesPath = path.posix.resolve(__dirname, './huff_modules');
 
-const { inputMap, macros, jumptables } = parser.parseFile('main.huff', pathToData);
+const proxy_constant = parseContract('proxy_constant.huff', modulesPath);
+const proxy_storage = parseContract('proxy_storage.huff', modulesPath);
 
-const {
-  data: { bytecode: macroCode },
-} = parser.processMacro('MAIN__PROXY', 0, [], macros, inputMap, jumptables);
-
-const {
-  data: { bytecode: compilerCode },
-} = parser.processMacro('MAIN__CONSTRUCTOR', 0, [], macros, inputMap, jumptables);
-
-const bytecode = compilerCode + macroCode;
-
-const contract = {
-  name: 'PROXY',
-  bytecode: `0x${macroCode}`,
-  deployedBytecode: `0x${bytecode}`,
-};
+const contracts = [{
+  name: 'proxy_constant',
+  bytecode: `0x${proxy_constant.bytecode}`,
+  runtimeBytecode: `0x${proxy_constant.runtime}`,
+}, {
+  name: 'proxy_storage',
+  bytecode: `0x${proxy_storage.bytecode}`,
+  runtimeBytecode: `0x${proxy_storage.runtime}`,
+}];
 
 fs.writeFileSync(
-  path.posix.resolve(__dirname, './proxy.json'),
-  JSON.stringify(contract)
+  path.posix.resolve(__dirname, './proxies.json'),
+  JSON.stringify(contracts)
 );
 
-compiler('proxy_project.json', pathToData);
+console.log('written bytecode to proxies.json');
 
-console.log('written bytecode to proxy.json');
+function parseContract(filename, pathToData) {
+  const { inputMap, macros, jumptables } = parser.parseFile(filename, pathToData);
+  runtime = parser.processMacro('RUNTIME', 0, [], macros, inputMap, jumptables).data.bytecode;
+  constructor = parser.processMacro('CONSTRUCTOR', 0, [], macros, inputMap, jumptables).data.bytecode;
+  bytecode = constructor + runtime;
+  return { constructor, runtime, bytecode };
+}
