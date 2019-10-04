72fe5f170757906fb07adb3676c8dff0e24db3e6
nsward <wardnick@protonmail.com>
2019-10-04 15:53:11 +0200

    Add base for proxy_2d

--- huff_tests/main.spec.js
+++ huff_tests/main.spec.js
@@ -1,11 +1,13 @@
-const chai = require('chai');
+// const chai = require('chai');
 const path = require('path');
 const BN = require('bn.js');
 
 const { Runtime } = require('../huff');
 
-const { expect } = chai;
+// const { expect } = chai;
 const pathToTestData = path.posix.resolve(__dirname, '../huff_modules');
+
+const { executeFn } = require('./helpers/helpers.js');
 
 describe('MAIN__PROXY', () => {
   let main;
@@ -13,5 +15,18 @@ describe('MAIN__PROXY', () => {
     main = new Runtime('main.huff', pathToTestData, true);
   });
 
-  it('exists', () => {});
+  it('exists', () => {
+
+  });
+});
+
+describe('MAIN__CONSTRUCTOR', () => {
+  let main_construcor;
+  before(async () => {
+    main_construcor = new Runtime('main.huff', pathToTestData, true);
+  });
+
+  it('deploys correctly', () => {
+
+  });
 });
